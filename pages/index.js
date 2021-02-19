cd9c71406737aff1c2de38cb05a43acb85a77dcf
Angelo Amadora <amadora.angelo@gmail.com>
2021-02-20 02:08:55 +0800

    changed height

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,55 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Particles from 'react-particles-js';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">amadora.tech!</a>
-        </h1>
+      <Particles 
+            params={{
+                particles: {
+                    number: {
+                        value: 400,
+                        density: {
+                            enable: true,
+                            value_area: 1000
+                        }
+                    },
+                    color: {
+                        value: '#fff'
+                    },
+                    opacity: {
+                        value: 0.5,
+                        anim: {
+                            enable: true
+                        }
+                    },
+                    size: {
+                        value: 7,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 3
+                        }
+                    },
+                    line_linked: {
+                        enable: false
+                    },
+                    move: {
+                        speed: 0.2
+                    }
+                 }    
+            }}
+            height={'99vh'}
+        />
+       
+        
 
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
