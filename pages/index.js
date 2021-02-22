d13997db1fd375a57a3095f7336579a935d30ec8
Angelo Amadora <amadora.angelo@gmail.com>
2021-02-22 18:12:26 +0800

    Added center text

--- pages/index.js
+++ pages/index.js
@@ -4,51 +4,52 @@ import Particles from 'react-particles-js';
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Particles 
-            params={{
-                particles: {
-                    number: {
-                        value: 400,
-                        density: {
-                            enable: true,
-                            value_area: 1000
+    <div className={styles.mainBackgroundColor}>
+        <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className={styles.welcome}>
+            <h1>Hello, I'm Angelo Amadora.<br/>I'm a full-stack web developer.</h1>
+        </div>
+        <Particles 
+                params={{
+                    particles: {
+                        number: {
+                            value: 100,
+                            density: {
+                                enable: true,
+                                value_area: 1000
+                            }
+                        },
+                        color: {
+                            value: '#fff'
+                        },
+                        opacity: {
+                            value: 0.5,
+                            anim: {
+                                enable: true
+                            }
+                        },
+                        size: {
+                            value: 4,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 3
+                            }
+                        },
+                        line_linked: {
+                            enable: false
+                        },
+                        move: {
+                            speed: 0.2
                         }
-                    },
-                    color: {
-                        value: '#fff'
-                    },
-                    opacity: {
-                        value: 0.5,
-                        anim: {
-                            enable: true
-                        }
-                    },
-                    size: {
-                        value: 7,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 3
-                        }
-                    },
-                    line_linked: {
-                        enable: false
-                    },
-                    move: {
-                        speed: 0.2
-                    }
-                 }    
-            }}
-            height={'99vh'}
-        />
-       
-        
+                    }    
+                }}
+                height={'100vh'}
+            />
+   
 
     </div>
   )
